a6947859f1f42d3737789b4956e16e21ca5cb614
first_name <ss329222@gmail.com>
2022-03-30 17:47:48 +0530

    Added:helloWorld

---
+++ src/main/java/com/example/abbs/hchalunga/HchalungaApplication.java
@@ -0,0 +1,14 @@
+package com.example.abbs.hchalunga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HchalungaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World ab mai run karega");
+		SpringApplication.run(HchalungaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abbs/hchalunga/HchalungaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abbs.hchalunga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HchalungaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
