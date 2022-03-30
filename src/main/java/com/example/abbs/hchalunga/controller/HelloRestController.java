a1c428281ec949e6cd612b5fd82ff1467b5bb38d
first_name <ss329222@gmail.com>
2022-03-30 18:24:30 +0530

    Added:Printing Hello World by using RestController

---
+++ src/main/java/com/example/abbs/hchalunga/controller/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.example.abbs.hchalunga.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    @RequestMapping(value = {"", "/", "/home"})
+    public String index() {
+        return "Hello From BridgeLabz";
+    }
+}
