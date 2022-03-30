6554fe3b789cda0f5d09ca3e6c5d1475fa4e8389
first_name <ss329222@gmail.com>
2022-03-30 18:11:57 +0530

    Added:DisplayMessageInWebPage

---
+++ src/main/java/com/example/abbs/hchalunga/controller/HelloWebController.java
@@ -0,0 +1,31 @@
+package com.example.abbs.hchalunga.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+    @Controller
+    public class HelloWebController {
+
+        /**
+         *
+         * @return hello.html page
+         */
+        @GetMapping("/web")
+        public String hello() {
+            return "hello";
+        }
+
+        /**
+         *
+         * @return message.html page
+         */
+
+        @GetMapping("/web/message")
+        public String message(Model model) {
+            model.addAttribute("message", "This is a custom message");
+            return "message";
+        }
+
+    }
